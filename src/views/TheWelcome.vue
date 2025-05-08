e91283e386b1d85a292dc527883a0f3d6d1d67eb
AissaniOussama <aissani.ous@gmail.com>
2025-05-08 17:48:14 +0200

    Milestone 2

--- src/views/TheWelcome.vue
+++ src/views/TheWelcome.vue
@@ -1,15 +1,30 @@
 <template>
   <h3 class="custom-font">Stickman Customizer</h3>
+  <div class="main-layout">
+    <div class="number-column">
+      <div
+        class="number-box"
+        v-for="(value, index) in numbers"
+        :key="'left-' + index"
+      >
+        <button @click="decrease(index)">←</button>
+        <span>{{ value }}</span>
+      </div>
+    </div>
 
-  <div class="number-column">
-    <div
-      class="number-box"
-      v-for="(value, index) in numbers"
-      :key="index"
-    >
-      <button @click="decrease(index)">←</button>
-      <span>{{ value }}</span>
-      <button @click="increase(index)">→</button>
+    <div class="canvas-wrapper">
+      <img src="@/assets/images/stickman-base.png" alt="Stickman" class="stickman" />
+    </div>
+
+    <div class="number-column">
+      <div
+        class="number-box"
+        v-for="(value, index) in numbers"
+        :key="'right-' + index"
+      >
+        <button @click="increase(index)">→</button>
+        <span>{{ value }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -35,25 +50,43 @@ h3.custom-font {
   text-align: center;
 }
 
+.main-layout {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  margin-top: 2rem;
+}
+
+.canvas-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.stickman {
+  width: 200px; /* oder eine andere passende Größe */
+  height: auto;
+}
+
 .number-column {
   display: flex;
   flex-direction: column;
   gap: 1rem;
   align-items: center;
-  margin-top: 2rem;
 }
 
 .number-box {
-  font-family: 'HoaxVandal', sans-serif;
-  font-size: 24px;
   display: flex;
   align-items: center;
-  gap: 1rem;
+  gap: 0.5rem;
+  font-family: 'HoaxVandal', sans-serif;
+  font-size: 20px;
 }
 
 button {
-  font-size: 20px;
-  padding: 0.3rem 0.6rem;
+  font-size: 18px;
+  padding: 0.4rem 0.6rem;
   cursor: pointer;
 }
 </style>
